7509ad56fb0d3c2f5b5eeb8b4f3f6703d19b8eb5
Roy Lenferink <lenferinkroy@gmail.com>
2017-02-22 11:30:07 -0500

    Code improvements for l4 inverted index
    Output index is sorted now

--- lesson4/inverted-index/mapper.py
+++ lesson4/inverted-index/mapper.py
@@ -11,13 +11,12 @@ reader = csv.reader(sys.stdin, delimiter='\t')
 
 delimiters = [' ', '.', ',', '!', '?', ':', ';', '"', '(', ')', '<', '>', '[', ']', '#', '$', '=', '-', '/']
 regex = '|'.join(map(re.escape, delimiters))
-print regex
 
 for line in reader:
 	nodeId = line[0]
-	if nodeId == "id":
+	if not nodeId.isdigit():
 		continue
 
 	body = re.split(regex, line[4])
 	for word in body:
-		print nodeId,"\t", word
+		print nodeId, "\t", word
